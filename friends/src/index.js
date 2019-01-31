6675f6c260ec264fe71abf54471320197f81cb72
rmejia-tajc <rmejia.tajc@gmail.com>
2019-01-31 09:51:41 -0700

    initial project setup. Create Redux boilerplate

---
+++ friends/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(()=>{}, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
